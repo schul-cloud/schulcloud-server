9cf8d83c6b2ad8e9c24a8739b07cd1cdbacc090a
tofixx <tobias.wollowski@hpi.de>
2021-06-18 06:21:51 +0000

    Deploying to gh-pages from @ hpi-schul-cloud/schulcloud-server@e971eff7cab5c5bd9a52583ee15aaeac05899fa3 🚀

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -240,13 +240,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/ServerModule.html" data-type="entity-link">ServerModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#controllers-links-module-ServerModule-6edb5b2fa5d13b7d6b8c729451ca3a60"' : 'data-target="#xs-controllers-links-module-ServerModule-6edb5b2fa5d13b7d6b8c729451ca3a60"' }>
+                                            'data-target="#controllers-links-module-ServerModule-d810cf27f3057bf4eac6e623ec82dc26"' : 'data-target="#xs-controllers-links-module-ServerModule-d810cf27f3057bf4eac6e623ec82dc26"' }>
                                             <span class="icon ion-md-swap"></span>
                                             <span>Controllers</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="controllers-links-module-ServerModule-6edb5b2fa5d13b7d6b8c729451ca3a60"' :
-                                            'id="xs-controllers-links-module-ServerModule-6edb5b2fa5d13b7d6b8c729451ca3a60"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="controllers-links-module-ServerModule-d810cf27f3057bf4eac6e623ec82dc26"' :
+                                            'id="xs-controllers-links-module-ServerModule-d810cf27f3057bf4eac6e623ec82dc26"' }>
                                             <li class="link">
                                                 <a href="controllers/ServerController.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">ServerController</a>
@@ -333,13 +333,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="classes/BusinessError.html" data-type="entity-link">BusinessError</a>
                             </li>
                             <li class="link">
+                                <a href="classes/Course.html" data-type="entity-link">Course</a>
+                            </li>
+                            <li class="link">
                                 <a href="classes/CourseInfo.html" data-type="entity-link">CourseInfo</a>
                             </li>
                             <li class="link">
                                 <a href="classes/CourseNews.html" data-type="entity-link">CourseNews</a>
-                            </li>
-                            <li class="link">
-                                <a href="classes/CourseTaskInfo.html" data-type="entity-link">CourseTaskInfo</a>
                             </li>
                             <li class="link">
                                 <a href="classes/CreateNewsParams.html" data-type="entity-link">CreateNewsParams</a>
@@ -348,16 +348,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="classes/ErrorResponse.html" data-type="entity-link">ErrorResponse</a>
                             </li>
                             <li class="link">
-                                <a href="classes/FileTaskInfo.html" data-type="entity-link">FileTaskInfo</a>
-                            </li>
-                            <li class="link">
                                 <a href="classes/GlobalErrorFilter.html" data-type="entity-link">GlobalErrorFilter</a>
                             </li>
                             <li class="link">
                                 <a href="classes/GlobalValidationPipe.html" data-type="entity-link">GlobalValidationPipe</a>
                             </li>
                             <li class="link">
-                                <a href="classes/LessonInfo.html" data-type="entity-link">LessonInfo</a>
+                                <a href="classes/Lesson.html" data-type="entity-link">Lesson</a>
                             </li>
                             <li class="link">
                                 <a href="classes/News.html" data-type="entity-link">News</a>
@@ -421,9 +418,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                             <li class="link">
                                 <a href="classes/UserInfoResponse.html" data-type="entity-link">UserInfoResponse</a>
-                            </li>
-                            <li class="link">
-                                <a href="classes/UserTaskInfo.html" data-type="entity-link">UserTaskInfo</a>
                             </li>
                             <li class="link">
                                 <a href="classes/ValidationErrorDetailResponse.html" data-type="entity-link">ValidationErrorDetailResponse</a>
